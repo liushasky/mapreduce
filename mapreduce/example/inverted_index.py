3230955d83f72735a28b1f44d8b2cb45ef07aba1
liushasky <liushaskymm@gmail.com>
2020-10-11 19:47:02 -0400

    with Ziang's guidance

---
+++ mapreduce/example/inverted_index.py
@@ -0,0 +1,35 @@
+import string
+import multiprocessing
+import re
+import os
+
+### The map function
+def map_words(filename):
+    """Read a file and return a sequence of (word, occurances) values.
+    """
+    STOP_WORDS = set([
+            'a', 'an', 'and', 'are', 'as', 'be', 'by', 'for', 'if', 'in', 
+            'is', 'it', 'of', 'or', 'py', 'rst', 'that', 'the', 'to', 'with',
+            ])
+    TR = str.maketrans(string.punctuation, ' ' * len(string.punctuation))
+    print('{} reading {}'.format(multiprocessing.current_process().name, filename))
+    output = []
+    with open(filename, 'rt') as f:
+        for line in f:
+            if line.lstrip().startswith('..'): # Skip rst comment lines
+                continue
+            line = line.translate(TR) # Strip punctuation
+            for word in line.split():
+                word = word.lower()
+                if word.isalpha() and word not in STOP_WORDS:
+                    output.append( (word, filename) )
+    return output
+
+
+### The reduce function
+def inverted_index(item):
+    """Convert the partitioned data for a word to a
+    tuple containing the word and the number of occurances.
+    """
+    word, doc_ids = item
+    return (word, set(doc_ids))
--- test/inverted_index_test.py
+++ test/inverted_index_test.py
@@ -1,19 +1,20 @@
-from mapreduce.example.word_count import *
+from mapreduce.example.inverted_index import *
 from mapreduce.api import *
 import operator
+
 ### Input/Output file path
 input_files = "*.txt"
-output_location = "output/wc_sample_result.txt"
+output_location = "output/inverted_index_sample_result.txt"
 
 ### Start testing
 cluster_id = init_cluster('localhost','9008')
-result = run_mapred(input_files, map_words, count_words, output_location)
+result = run_mapred(input_files, map_words, inverted_index, output_location)
 destroy_cluster(cluster_id)
 
 result.sort(key=operator.itemgetter(1))
 result.reverse()
 ## Report
-print('\nTOP 20 WORDS BY FREQUENCY\n')
+print('\nTOP 20 WORDS BY LOCATION\n')
 top20 = result[:20]
 for word, count in top20:
     print('{}: {}'.format(word, count))
